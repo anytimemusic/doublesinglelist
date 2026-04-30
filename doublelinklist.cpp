0f95490f3017af6072711fd5323786a46cda0ba4
anytimemusic <bima220711@gmail.com>
2026-04-30 11:45:24 +0700

    bkin traverse

--- doublelinklist.cpp
+++ doublelinklist.cpp
@@ -113,3 +113,23 @@ if (START == NULL || nim <= START->noMhs)
         cout << "Record with roll number " << rollNo << " deleted" << endl;
     }
 
+   void traverse()
+    {
+        if (START == NULL)
+        {
+            cout << "List is empty" << endl;
+            return;
+        }
+
+        Node *currentNode = START;
+
+        cout << "Records in ascending order of roll number are:\n";
+
+        while (currentNode != NULL)
+        {
+            cout << currentNode->noMhs << " ";
+            currentNode = currentNode->next;
+        }
+    }
+
+    
